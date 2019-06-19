5e486c9e5776510957a22cc8bf9586078ed77752
Luis Dasta <ldasta786@gmail.com>
2019-06-19 11:05:08 -0500

    Add project files.

---
+++ DelegatesEvents/UserProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesEvents
+{
+    public class UserProcessor
+    {
+        public void ProcessUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
