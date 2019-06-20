fd3c2dcbd794562dd4bdf9b2aef177124f0c45ea
Luis Dasta <ldasta786@gmail.com>
2019-06-20 09:04:24 -0500

    quick demo of text service no tests

--- DelegatesEvents/Program.cs
+++ DelegatesEvents/Program.cs
@@ -9,8 +9,9 @@ namespace DelegatesEvents
             Console.WriteLine("Running Application!");
             var processor = new UserProcessor();
             var emailService = new EmailNotificationService();
+            var textService = new TextMessageNotificationService();
             processor.UserProcessed += emailService.OnUserProcessed;
-
+            processor.UserProcessed += textService.OnUserProcessed;
             processor.ProcessUser();
         }
     }
---
+++ DelegatesEvents/TextMessageNotificationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesEvents
+{
+    public class TextMessageNotificationService
+    {
+        public void OnUserProcessed(object sender, EventArgs args)
+        {
+            SendText();
+        }
+
+        public void SendText()
+        {
+            Console.WriteLine("Sending Text Message...");
+        }
+    }
+}
